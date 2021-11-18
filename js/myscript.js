c851ee9d6a831ab25b8afc37670e589ca79c84c1
Alessandro <luisialessandro95@gmail.com>
2021-11-18 16:01:41 +0100

    riempimento griglia

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,43 @@
-alert('Ciaone');
+// alert('Ciaone');
+
+const playBtn = document.getElementById('play');
+// console.log(playBtn);
+
+playBtn.addEventListener('click', function(){
+
+    const bigBox = document.getElementById('big-box');
+    bigBox.innerHTML = '';
+
+    const levelSelected = parseInt(document.getElementById('level').value);
+    // console.log(levelSelected);
+    let boxNumber;
+
+    switch (levelSelected){
+        case 1:
+            boxNumber = 100;
+            break;
+        case 2:
+            boxNumber = 81;
+            break;
+        case 3:
+            boxNumber = 49;
+            break;
+    }
+
+    let boxOnARow = Math.sqrt(boxNumber);
+    // console.log(boxNumber);
+    // console.log(boxOnARow);
+    
+    for (let i = 1; i <= boxNumber; i++){
+
+        const box = document.createElement('div')
+        const size = `calc(100% / ${boxOnARow})`;
+        box.classList.add('cubox');
+        box.style.width = size;
+        box.style.height = size;
+
+        box.innerHTML = i
+
+        bigBox.appendChild(box);
+    }
+})
