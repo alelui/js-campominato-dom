0111c33341befc6c626cd94f1daa04d457baa304
Alessandro <luisialessandro95@gmail.com>
2021-11-18 16:20:39 +0100

    cercando di appendere

--- js/myscript.js
+++ js/myscript.js
@@ -29,15 +29,26 @@ playBtn.addEventListener('click', function(){
     // console.log(boxOnARow);
     
     for (let i = 1; i <= boxNumber; i++){
-
+        //Crea il div dei quadrati
         const box = document.createElement('div')
         const size = `calc(100% / ${boxOnARow})`;
+        //aggiunge al div dei quadrati la classe del css
         box.classList.add('cubox');
+        //Aggunge lunghezza ed altezza con style inline
         box.style.width = size;
         box.style.height = size;
 
         box.innerHTML = i
-
+        //Inserisce l'lemento box creata, con tutte le modifiche, all interno del padre
         bigBox.appendChild(box);
     }
+
+    box.addEventListener('click', function(){
+        this.classList.add('active');
+    })
 })
+
+
+function (num){
+    
+}
